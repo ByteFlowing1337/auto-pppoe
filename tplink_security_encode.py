48b67c00f81851da94afa963277cd9021de1e6c4
ByteFlow <fakeshadow1337@gmail.com>
2026-03-14 10:59:33 +0800

    Add detailed docstring with reverse-engineered JavaScript code for tplink_security_encode

--- tplink_security_encode.py
+++ tplink_security_encode.py
@@ -1,3 +1,25 @@
+"""Reverse engineered from the TPLINK router's javascript code
+Raw code:
+
+    this.orgAuthPwd = function(a) {
+        return this.securityEncode(a, "RDpbLfCPsJZ7fiv", "yLwVl0zKqws7LgKPRQ84Mdt708T1qQ3Ha7xv3H7NyU84p21BriUWBU43odz3iP4rBL3cD02KZciXTysVXiV8ngg6vL48rPJyAUw0HurW20xqxv9aYb4M9wK1Ae0wlro510qXeU07kV57fQMc8L6aLgMLwygtc0F10a0Dg70TOoouyFhdysuRMO51yY5ZlOZZLEal1h0t9YQW0Ko7oBwmCAHoic4HYbUyVeU3sfQ1xtXcPcf1aT303wAQhv66qzW")
+    }
+    ;
+    this.securityEncode = function(a, c, b) {
+        var d = "", e, f, g, h, k = 187, l = 187;
+        f = a.length;
+        g = c.length;
+        h = b.length;
+        e = f > g ? f : g;
+        for (var m = 0; m < e; m++)
+            l = k = 187,
+            m >= f ? l = c.charCodeAt(m) : m >= g ? k = a.charCodeAt(m) : (k = a.charCodeAt(m),
+            l = c.charCodeAt(m)),
+            d += b.charAt((k ^ l) % h);
+        return d
+    }
+    ;
+"""
 def tplink_security_encode(password):
     key = "RDpbLfCPsJZ7fiv"
     dictionary = "yLwVl0zKqws7LgKPRQ84Mdt708T1qQ3Ha7xv3H7NyU84p21BriUWBU43odz3iP4rBL3cD02KZciXTysVXiV8ngg6vL48rPJyAUw0HurW20xqxv9aYb4M9wK1Ae0wlro510qXeU07kV57fQMc8L6aLgMLwygtc0F10a0Dg70TOoouyFhdysuRMO51yY5ZlOZZLEal1h0t9YQW0Ko7oBwmCAHoic4HYbUyVeU3sfQ1xtXcPcf1aT303wAQhv66qzW"
